f75667282ad9d72d7283d02320280963fd72b19c
Hery Martin <scorpionxii@gmail.com>
2017-06-02 10:35:11 +0200

    Search service!

--- retroshop-client/src/components/product-create/product-create.component.ts
+++ retroshop-client/src/components/product-create/product-create.component.ts
@@ -3,13 +3,17 @@ import {SessionService} from "../../services/session/session.service";
 import {Router} from "@angular/router";
 import {ProductService} from "../../services/product/product.service";
 import {environment} from "../../environments/environment";
+import {FileUploader} from "ng2-file-upload";
 
 @Component({
   selector: 'app-product',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
+
 export class ProductCreateComponent implements OnInit {
+
+  uploader = new FileUploader({ url: `${environment.serverUrl}/api/products` });
 
   formInfo = {
     title: '',
@@ -36,6 +40,16 @@ export class ProductCreateComponent implements OnInit {
     this.filename = "Picture loaded...";
     console.log(this.filename);
   }
+
+  // submit(){
+  //   this.uploader.onBuildItemForm = (item, form) => {
+  //     form.append('username', this.formInfo.username);
+  //     form.append('password', this.formInfo.password);
+  //     form.append('fullname', this.formInfo.fullname);
+  //     form.append('lat', this.location.lat);
+  //     form.append('lng', this.location.lng);
+  //   };
+  // }
 
   submit() {
     this.formInfo.seller = this.user._id;
